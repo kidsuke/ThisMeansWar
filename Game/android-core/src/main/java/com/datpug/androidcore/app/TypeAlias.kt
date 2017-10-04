d918b93aaa7ca0b2e2a8281ac4a1394462696371
Phuc Ho (Mia) <hongphuc0226@gmail.com>
2017-10-05 02:51:39 +0300

    Fix project structure

--- Game/android-core/src/main/java/com/datpug/androidcore/app/TypeAlias.kt
+++
@@ -1,6 +0,0 @@
-package com.datpug.androidcore.app
-
-/**
- * Created by longvu on 17/09/2017.
- */
-typealias GeneralAbstractFragment = AbstractFragment<*>
--- Game/android-core/src/main/java/com/datpug/androidcore/app/ViewType.kt
+++
@@ -1,8 +0,0 @@
-package com.datpug.androidcore.app
-
-/**
- * Created by longvu on 16/09/2017.
- */
-interface ViewType {
-    fun getLayoutId(): Int
-}
